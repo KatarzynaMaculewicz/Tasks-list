524ff6472be0ff33584d76491be7965ce0ae8cbc
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2023-03-22 12:26:40 +0000

    toggle task done

--- js/script.js
+++ js/script.js
@@ -18,19 +18,25 @@ console.log("hello");
 
     render();
   };
-  
+
   const removeTask = (taskIndex) => {
-    tasks.splice(taskIndex,1);
-          render();
+    tasks.splice(taskIndex, 1);
+    render();
   };
-  
+
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+  };
+
   const render = () => {
     let htmlString = "";
 
     for (const task of tasks) {
       htmlString += `
-          <li${task.done ? " style=\"text-decoration: line-through\"" : ""}
+          <li${task.done ? ' style="text-decoration: line-through"' : ""}
           >
+          <button class="js-done">zrobione?</button>
           <button class="js-remove">usuń</button>
             ${task.content}
           </li>
@@ -39,24 +45,32 @@ console.log("hello");
     document.querySelector(".js-tasks").innerHTML = htmlString;
 
     const removeButtons = document.querySelectorAll(".js-remove");
-   
+
     removeButtons.forEach((removeButton, index) => {
-      removeButton.addEventListener("click",() => {
+      removeButton.addEventListener("click", () => {
         removeTask(index);
       });
     });
-  };
-  
-const onFormSubmit = (event) => {
-  event.preventDefault();
 
-  const newTaskContent = document.querySelector(".js-newTask").value.trim();
-  
-  if (newTaskContent === ""){
-    return;
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+      });
+    });
   };
 
-  addNewTask(newTaskContent);
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
   };
 
   const init = () => {
