a480a2990b5b920a76caed8512e74335f173db33
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2023-03-22 10:57:39 +0000

    remove tasks

--- js/script.js
+++ js/script.js
@@ -11,6 +11,19 @@ console.log("hello");
     },
   ];
 
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  };
+  
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex,1);
+          render();
+  };
+  
   const render = () => {
     let htmlString = "";
 
@@ -18,21 +31,22 @@ console.log("hello");
       htmlString += `
           <li${task.done ? " style=\"text-decoration: line-through\"" : ""}
           >
+          <button class="js-remove">usuń</button>
             ${task.content}
           </li>
           `;
     }
     document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+   
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click",() => {
+        removeTask(index);
+      });
+    });
   };
   
-const addNewTask = (newTaskContent) => {
-  tasks.push({
-    content: newTaskContent,
-  });
-
-  render();
-};
-
 const onFormSubmit = (event) => {
   event.preventDefault();
 
